a3d4e876303e83be8dd780e03175050dd4d540af
mdeleonbalbiani <martinadeleonbalbiani@gmail.com>
2022-01-25 18:58:24 -0300

    Desadio 4 / Clase 8

---
+++ API_RESTful-Clase8/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const productosRouter = require('./routes');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api/productos', productosRouter);
+app.use('/static', express.static('public'))
+
+app.listen(8080,() => {
+    console.log('Servidor corriendo en el puerto 8080')
+}).on( 'error',(err) => console.log(err))
